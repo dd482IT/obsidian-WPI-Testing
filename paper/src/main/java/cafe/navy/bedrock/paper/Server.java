eca1b4796465c288c9899a1b4254f9b823399468
kadenscott <github.com@kaden.sh>
2022-08-20 19:22:57 -0400

    docs: jd Server

--- paper/src/main/java/cafe/navy/bedrock/paper/Server.java
+++ paper/src/main/java/cafe/navy/bedrock/paper/Server.java
@@ -5,6 +5,7 @@ import cafe.navy.bedrock.paper.realm.Realm;
 import cafe.navy.bedrock.paper.realm.WorldRealm;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
@@ -28,24 +29,47 @@ public class Server {
     private final @NonNull Map<UUID, Realm> realms;
     private boolean enabled = false;
 
+    /**
+     * Constructs {@code Server}.
+     *
+     * @param plugin the plugin that instantiated this server
+     */
     public Server(final @NonNull JavaPlugin plugin) {
         this.plugin = plugin;
         this.playerManager = new PlayerManager(this.plugin);
         this.realms = new HashMap<>();
     }
 
+    /**
+     * Returns whether or not the server has been enabled.
+     *
+     * @return true or false
+     */
     public boolean enabled() {
         return this.enabled;
     }
 
+    /**
+     * Returns the {@link PlayerManager}.
+     *
+     * @return the player manager
+     */
     public @NonNull PlayerManager players() {
         return this.playerManager;
     }
 
+    /**
+     * Returns the list of registered {@link Realm}s.
+     *
+     * @return the realms list
+     */
     public @NonNull List<Realm> realms() {
         return List.copyOf(this.realms.values());
     }
 
+    /**
+     * Enables {@code Server}.
+     */
     public void enable() {
         this.playerManager.enable();
         for (final World world : Bukkit.getWorlds()) {
@@ -55,17 +79,21 @@ public class Server {
         this.enabled = true;
     }
 
+    /**
+     * Disables {@code Server}.
+     */
     public void disable() {
         this.playerManager.disable();
         this.realms.clear();
         this.enabled = false;
     }
 
-    private void registerRealm(final @NonNull Realm realm) {
-        realm.enable();
-        this.realms.put(realm.uuid(), realm);
-    }
-
+    /**
+     * {@link BukkitRunnable#runTaskTimer} alias
+     * @param runnable the runnable
+     * @param time time delay
+     * @return the task
+     */
     public @NonNull BukkitTask registerTimedTask(final @MonotonicNonNull Runnable runnable,
                                                  final int time) {
         return new BukkitRunnable() {
@@ -76,6 +104,12 @@ public class Server {
         }.runTaskTimer(this.plugin, 0, time);
     }
 
+    /**
+     * {@link BukkitRunnable#runTaskLater} alias
+     * @param runnable the runnable
+     * @param time time delay
+     * @return the task
+     */
     public @NonNull BukkitTask registerDelayedTask(final @MonotonicNonNull Runnable runnable,
                                                    final int time) {
         return new BukkitRunnable() {
@@ -84,5 +118,10 @@ public class Server {
                 runnable.run();
             }
         }.runTaskLater(this.plugin, time);
+    }
+
+    private void registerRealm(final @NonNull Realm realm) {
+        realm.enable();
+        this.realms.put(realm.uuid(), realm);
     }
 }
