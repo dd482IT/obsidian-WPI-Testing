6743c3dea803bd3da0acad09da49ce821b199d27
kadenscott <github.com@kaden.sh>
2022-08-20 21:11:45 -0400

    fix: gradle build script, add plugin.yml

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -7,10 +7,19 @@ plugins {
 }
 
 dependencies {
-    paperDevBundle("1.19.1-R0.1-SNAPSHOT")
-    compileOnly(libs.cloud.core)
-    compileOnly(libs.cloud.paper)
-    compileOnly(libs.corn.minecraft.paper) {
+    paperDevBundle("1.19.2-R0.1-SNAPSHOT")
+    implementation(libs.cloud.core)
+    implementation(libs.cloud.paper)
+    implementation(libs.corn.minecraft.paper) {
         exclude(group="io.papermc.paper", module="paper-api")
+    }
+}
+
+tasks {
+    assemble {
+        dependsOn(reobfJar)
+    }
+    reobfJar {
+        outputJar.set(rootProject.layout.buildDirectory.file("libs/${project.name}.jar"))
     }
 }
