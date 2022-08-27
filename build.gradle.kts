fbdd7e43c7546ed985f338f56bbea3fa80773aa6
kadenscott <github.com@kaden.sh>
2022-08-26 23:02:46 -0400

    fix: update paper repo URL

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ subprojects {
         mavenCentral()
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
         maven("https://jitpack.io")
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://repo.broccol.ai/releases")
     }
 
